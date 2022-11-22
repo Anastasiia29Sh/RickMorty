8b4c6b0ed46dbf5fcbee46b238e73e3b55e2e768
Anastasiia29Sh <nastya.sharcko@yandex.ru>
2022-11-22 20:27:23 +0300

    start

---
+++ RickMortyGroupProject/Characters/RadioButtonController.swift
@@ -0,0 +1,13 @@
+//
+//  RadioButtonController.swift
+//  RickMortyGroupProject
+//
+//  Created by MacStud on 16.11.2022.
+//  Copyright © 2022 MacStud. All rights reserved.
+//
+
+import UIKit
+
+class RadioButtonController: NSObject {
+
+}
